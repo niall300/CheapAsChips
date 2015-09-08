8f30cf83c30d85d0e973d711d7bf36724e44aeca
niall300 <niall300@yahoo.co.uk>
2015-09-08 19:55:19 +0100

    commitment

---
+++ CheapAsChips/CheapAsChips/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheapAsChips.MvcApplication" Language="C#" %>
---
+++ CheapAsChips/CheapAsChips/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheapAsChips.Startup))]
+namespace CheapAsChips
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
