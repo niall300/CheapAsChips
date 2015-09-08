39fe013b0bb80aa2d2726c4fb697b793e2240f65
niall300 <niall300@yahoo.co.uk>
2015-09-08 20:15:22 +0100

    recipe page and model classes added

---
+++ CheapAsChips/CheapAsChips/Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheapAsChips1.Models
+{
+    public class Ingredient
+    {
+        public int recipeId { get; set; }
+        public String Name { get; set; }
+        public String Measure { get; set; }
+        public Double Quantity { get; set; }
+
+
+    }
+}
---
+++ CheapAsChips/CheapAsChips/Models/Instructions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheapAsChips1.Models
+{
+    public class Instructions
+    {
+        public int RecipeId { get; set; }
+        public string[] Steps { get; set; }
+    }
+}
